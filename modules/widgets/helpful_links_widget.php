2c7f260ad793380d3d61e70fcb532a1dcda0b280
luke <luke.ketchen@gmail.com>
2022-06-20 20:20:33 +1000

    widget - add json data

--- modules/widgets/helpful_links_widget.php
+++ modules/widgets/helpful_links_widget.php
@@ -1,10 +1,9 @@
 
 
-<p>Some helpful advice is located here:</p>
+<?= $content[0]['question']; ?>
+
 <ul>
-	<li>Some information that might help can be found here - <a href="http://google.com" target="_blank" rel="noopener noreferrer">Google</a></li>
-	<li>For help creating modules, watch this short video - <a href="http://google.com" target="_blank" rel="noopener noreferrer">Google</a></li>
-	<li>Creating a post - <a href="http://google.com" target="_blank" rel="noopener noreferrer">Google</a></li>
-	<li>If a maps section is not working - <a href="http://google.com" target="_blank" rel="noopener noreferrer">Google</a></li>
-	<li><a href="https://google.com" target="_blank" style="text-decoration: underline; font-weight:strong;">Link to the help page</a></li>
+	<?php foreach($content[1]['items'] as $item) : ?>
+		<li><?= $item['content']; ?> <a href="<?= $item['link_address']; ?>"  target="_blank" rel="noopener noreferrer"><?= $item['link_text']; ?></a></li>
+	<?php endforeach; ?>
 </ul>
--- modules/widgets/office_details_widget.php
+++ modules/widgets/office_details_widget.php
@@ -1,11 +1,11 @@
 
 
-<p style="color: green;font-size: 18px;"><strong>Our office hours</strong></p>
+<p style="color: green;font-size: 18px;"><strong><?= $content[0]['question']; ?></strong></p>
+
 <ul>
-	<li>Mon: 8:30 - 5</li>
-	<li>Tue: 8:30 - 5</li>
-	<li>Wed: 8:30 - 5</li>
-	<li>Thu: 8:30 - 5</li>
-	<li>Fri: 8:30 - 5</li>
+	<?php foreach($content[1]['items'] as $item) : ?>
+		<li><?= $item; ?></li>
+	<?php endforeach; ?>
 </ul>
-<p>Contact <a href="mailto:hello@gmail.com">Company Name</a> when questions arise.</p>
+
+<p>Contact <a href="mailto:<?= $content[2]['footer_text']; ?>"><?= $content[2]['footer_link_text']; ?></a> <?= $content[2]['footer_text']; ?></p>
