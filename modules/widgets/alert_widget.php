782234e4560fd66a850532120a6f1f5747c450dd
luke ketchen <luke@efront.com.au>
2022-07-14 08:15:14 +1000

    Widgets - test info from api

--- modules/widgets/alert_widget.php
+++ modules/widgets/alert_widget.php
@@ -1,2 +1,21 @@
 
-<h2><?= $content[0]['content']; ?></h2>
+<!-- <h2><?php //echo $content[0]['content']; ?></h2> -->
+
+
+
+
+<?php
+	$connected_url = 'http://boiler.local/wp-json/wp/v2/fdcpt_widget/137';
+
+?>
+
+<?php
+$response = wp_remote_get($connected_url);
+$api_response = json_decode( wp_remote_retrieve_body( $response ), true );
+
+//print_r($api_response['content']['rendered']);
+?>
+
+<?php if(!empty($api_response)) :?>
+	<?=$api_response['content']['rendered'];?>
+<?php endif; ?>
